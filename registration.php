8ac287ca33975d32a3bc886a4de10a8c542b5fef
Durga Shankar Gupta <dsguptas@gmail.com>
2019-11-21 22:37:28 +0545

    init module

---
+++ registration.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Ace Extensions
+ *
+ * @category   Ace
+ * @package    Ace_B2bRegistration
+ * @author     Durga Shankar Gupta (dsguptas@gmail.com)
+ * @copyright  Copyright (c) 2019 Ace Extensions ( http://aceextensions.com )
+ */
+// @codingStandardsIgnoreFile
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Ace_B2bRegistration',
+    __DIR__
+);
